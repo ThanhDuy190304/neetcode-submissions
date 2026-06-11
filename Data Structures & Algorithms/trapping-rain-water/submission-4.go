8434e68d5ae3d6b9c2f4fb116c6ca2ee8c633e55
Thanh Duy <166845215+ThanhDuy190304@users.noreply.github.com>
2026-06-11 13:55:01 +0700

    Add: trapping-rain-water - submission-4

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-4.go
@@ -0,0 +1,45 @@
+func min(a, b int) int {
+    if a < b {
+        return a
+    }
+    return b
+}
+
+func trap(height []int) int {
+	result := 0
+	l, r := 0, len(height) - 1
+	for (l < r){
+		if (height[l] == 0){
+			l++
+			continue
+		}
+		if (height[r] == 0){
+			r--
+			continue
+		}
+
+		h := min(height[l], height[r])
+		fmt.Println("l =", l, "r =", r, "h =", h)
+
+
+		for(l < r && height[l] <= height[r]){
+			if(height[l] > h){
+				break
+			}
+			result += h - height[l]
+			l++
+		}
+
+		for(l < r && height[l] > height[r]){
+			if(height[r] > h){
+				break
+			}
+			fmt.Println("height r =", height[r])
+			result += h - height[r]
+			r --
+		}
+	}
+
+	return result
+
+}
